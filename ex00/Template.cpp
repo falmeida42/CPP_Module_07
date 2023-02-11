d2feacb12eedc8d0695797c3437adb1f397be4e7
Flavio Almeida <falmeida@c2r8s1.42lisboa.com>
2023-02-11 19:04:41 +0000

    cpp07

---
+++ ex00/Template.cpp
@@ -0,0 +1,16 @@
+#include "Template.hpp"
+
+Template<class T>::Template(/* args */)
+{
+
+}
+
+Template<class T>::~Template()
+{
+
+}
+
+Template<class T>::swap(T &a, T &b)
+{
+
+}
---
+++ ex00/Template.hpp
@@ -0,0 +1,16 @@
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+#include <iostream>
+
+template<class T>
+class Template
+{
+    private:
+        /* data */
+    public:
+        swap(T &a, T &b);
+        Template(/* args */);
+        ~Template();
+};
+
+#endif
